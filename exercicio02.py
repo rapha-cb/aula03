f6cf426ecbd6c489998e54720447105d151a06a0
raphael cordeiro <raphaelcbpereira@gmial.com>
2025-04-01 14:42:09 -0300

    fazendo print das operações matemáticas

---
+++ exercicio02.py
@@ -0,0 +1,12 @@
+numero1 = int(input("digite o primeiro número: "))
+numero2 = int(input("digite o segundo número: "))
+
+soma= numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao= numero1 * numero2
+divisao= numero1/numero2
+
+print(f"a soma de {numero1} + {numero2}= {soma}")
+print(f"a subtração de {numero1} - {numero2}= {subtracao}")
+print(f"a multiplicação de {numero1} * {numero2}= {multiplicacao}")
+print(f"a divisão de {numero1} / {numero2}= {divisao}")
