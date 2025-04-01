962c45cdf148af971c9810c649adf3c04829222c
raphael cordeiro <raphaelcbpereira@gmial.com>
2025-04-01 15:56:24 -0300

    novo calculo de media com if else

---
+++ exercicio05.py
@@ -0,0 +1,11 @@
+nota1= float(input("digite a primeira nota: "))
+nota2= float(input("digite a segunda nota: "))
+nota3= float(input("digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+if media >= 7:
+    print("você foi aprovado")
+else:
+    print("você foi reprovado")
+    
