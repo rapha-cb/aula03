afe47c44de7c446342c2fae8dc5cd063e7d7563e
raphael cordeiro <raphaelcbpereira@gmial.com>
2025-04-01 13:58:25 -0300

    código de informações do usuario

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+nome = input("digite seu nome: ")
+idade = input("agora digite sua idade: ")
+salario = float(input("inorme seu salário: "))
+
+print(f"informações:\n {nome} \n {idade} \n{salario}")
