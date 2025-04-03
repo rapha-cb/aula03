67475de51ccdb0296ebdca83a03b8ccb51a3ffd9
raphael cordeiro <raphaelcbpereira@gmial.com>
2025-04-03 14:38:17 -0300

    atualizando código da gasolina e código de par ou impar

--- exercicio07.py
+++ exercicio07.py
@@ -2,10 +2,11 @@ combustivel= input("Qual o tipo de gasolina? G para gasolina e E para etanol: ")
 litragem= float(input("agora me informe quantos litros voce abasteceu: "))
 g= 5.8
 e= 4.9
-if combustivel == "G":
-    litragem = litragem * g
+valor = 0
+if combustivel == "G" or combustivel == "g":
+    valor = litragem * g
+elif combustivel == "E" or combustivel == "e":
+        vaolor = litragem * e
 else:
-    if combustivel == "E":
-        litragem = litragem * e
-
-print(f"voce irá pagar R${litragem:.2f}")
+    print("tipo de combustível inválido")
+print(f"voce irá pagar R${valor:.2f}")
---
+++ exercicio08.py
@@ -0,0 +1,5 @@
+numero = int(input("digite um número: "))
+if numero % 2 == 0:
+    print("esse número é par")
+else:
+    print("esse número é ímpar")
